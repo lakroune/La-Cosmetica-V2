8a4db0508e716e6f43afac5ab3174be4abb29ebd
lakroune <lakroune00@gmail.com>
2026-03-30 15:15:39 +0100

    Refactor App component to update routes for login, registration, and home; replace CategoryPage and ProductPage with new admin structure.

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
 import { Routes, Route } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import LoginPage from "./auth/LoginPage";
+import ProductPage from "./admin/ProductPage";
 import RegisterPage from "./auth/RegisterPage";
-import CategoryPage from "./auth/admin/CategoryPage";
-import ProductPage from "./auth/admin/ProductPage";
+import CategoryPage from "./admin/CategoryPage";
+import HomePage from "./client/HomePage";
+ 
 
 function App() {
   return (
     <>
       <Toaster />
       <Routes>
-        <Route path="/login" element={<LoginPage />} />
+        <Route path="/l" element={<LoginPage />} />
         <Route path="/p" element={<ProductPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/" element={<CategoryPage />} />
+        <Route path="/r" element={<RegisterPage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/c" element={<CategoryPage />} />
       </Routes>
     </>
   );    
